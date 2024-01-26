78282cb2e853b1bc25c341e835d50dc2e4c8dffe
gary <henry.gary@hotmail.com>
2024-01-26 19:27:13 +0100

    feat: Add the executable for the code to run on a server

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 )
 
+const PORT = "8080"
+
 func main() {
-	// TODO: Mettre ce truc en production avec la base de donnee
+	port_formatted := fmt.Sprintf(":%s", PORT)
 	store, err := NewStore()
 	defer store.db.Close()
 	if err != nil {
@@ -17,4 +20,11 @@ func main() {
 	store.FillDB()
 	id, queryRes := store.CheckDB(1)
 	fmt.Printf("Value of the column with the value %d : %s\n", id, queryRes)
+	// TODO: On teste d'abord le serveur et ensuite on gere la connection avec la base de donnee
+	if err := http.ListenAndServe(port_formatted, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Je tape dans le serveur")
+		fmt.Fprintln(w, "Le serveur renvoit la reponse suivante")
+	})); err != nil {
+		log.Fatal("Cannot connect to the server - %s", err)
+	}
 }
