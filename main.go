3dcaa14e61173057acd7f59c68e2a6df3a67286c
gary <henry.gary@hotmail.com>
2024-01-27 12:08:48 +0100

    fix: change the port from using localhost to 0.0.0.0

--- main.go
+++ main.go
@@ -21,7 +21,9 @@ func main() {
 	id, queryRes := store.CheckDB(1)
 	fmt.Printf("Value of the column with the value %d : %s\n", id, queryRes)
 	// TODO: On teste d'abord le serveur et ensuite on gere la connection avec la base de donnee
-	http.ListenAndServe(port_formatted, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	_ = port_formatted
+	// http.ListenAndServe(port_formatted, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.ListenAndServe("0.0.0.0:1234", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Le serveur vient de recevoir une requete")
 		fmt.Fprintln(w, "Le serveur renvoie la reponse suivante")
 	}))
